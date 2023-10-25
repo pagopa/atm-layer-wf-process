d22bff857bb4bbc60472f086e394a57f19ef4777
pasquales <pasqualesansonna@aurigaspa.com>
2023-10-25 17:27:53 +0200

    Native changes

--- src/main/java/it/pagopa/atmlayer/wf/process/util/Properties.java
+++ src/main/java/it/pagopa/atmlayer/wf/process/util/Properties.java
@@ -1,11 +1,9 @@
 package it.pagopa.atmlayer.wf.process.util;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithName;
 import jakarta.enterprise.context.ApplicationScoped;
 
-@RegisterForReflection
 @ApplicationScoped
 @ConfigMapping(prefix = "wf-process")
 public interface Properties {

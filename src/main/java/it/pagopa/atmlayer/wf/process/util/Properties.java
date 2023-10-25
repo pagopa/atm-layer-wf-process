c38fb3cf57a0d173cc098baa3c261647dbe56045
pasquales <pasqualesansonna@aurigaspa.com>
2023-10-25 16:12:50 +0200

    Update props for native build

--- src/main/java/it/pagopa/atmlayer/wf/process/util/Properties.java
+++ src/main/java/it/pagopa/atmlayer/wf/process/util/Properties.java
@@ -1,9 +1,11 @@
 package it.pagopa.atmlayer.wf.process.util;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithName;
 import jakarta.enterprise.context.ApplicationScoped;
 
+@RegisterForReflection
 @ApplicationScoped
 @ConfigMapping(prefix = "wf-process")
 public interface Properties {

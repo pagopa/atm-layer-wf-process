398b541ed674d6359dd9c301da6347bfeb1ab5f6
pasquales <pasqualesansonna@aurigaspa.com>
2023-11-17 10:49:35 +0100

    Integration test

---
+++ src/test/java/it/pagopa/atmlayer/wf/process/test/EnvironmentTestServicesResource.java
@@ -0,0 +1,44 @@
+package it.pagopa.atmlayer.wf.process.test;
+
+import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
+import io.quarkus.test.junit.QuarkusTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.Map;
+
+@QuarkusTest
+public class EnvironmentTestServicesResource {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnvironmentTestServicesResource.class);
+
+    public static class DockerCompose implements QuarkusTestResourceLifecycleManager {
+        private DockerComposeContainer<?> dockerComposeContainer;
+
+        @Override
+        public Map<String, String> start() {
+
+            dockerComposeContainer = new DockerComposeContainer<>(new File("src/test/resources/integration-test/docker-compose.yml"))
+                    .withExposedService("minio", 9000)
+                    .withExposedService("mockoon", 3000);
+
+            dockerComposeContainer.withLogConsumer("minio", new Slf4jLogConsumer(LOGGER).withPrefix("minio"));
+            dockerComposeContainer.withLogConsumer("mockoon", new Slf4jLogConsumer(LOGGER).withPrefix("mockoon"));
+
+            dockerComposeContainer.start();
+
+            return Collections.emptyMap();
+        }
+
+        @Override
+        public void stop() {
+            if (dockerComposeContainer != null) {
+                dockerComposeContainer.stop();
+            }
+        }
+    }
+
+}
---
+++ src/test/resources/integration-test/run-postman.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+newman run /postman/Integration_test_process.postman_collection.json -r cli,json,htmlextra \
+--reporter-json-export output/result.json --reporter-htmlextra-export output/result.html \
+--env-var "baseUrl=http://host.testcontainers.internal:8086"

137760d42c68fd281e17c1a1a17109e75d075747
francescoa <francescoa@VDI-EC-013.aurigaactive.aurigainformatica.it>
2024-03-27 09:15:10 +0100

    TRANSACTIONS_SERVICE_ADDRESS variable

--- src/main/java/it/pagopa/atmlayer/wf/process/client/transactions/TransactionsServiceRestClient.java
+++ src/main/java/it/pagopa/atmlayer/wf/process/client/transactions/TransactionsServiceRestClient.java
@@ -1,6 +1,5 @@
 package it.pagopa.atmlayer.wf.process.client.transactions;
 
-import org.eclipse.microprofile.rest.client.annotation.ClientHeaderParam;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import it.pagopa.atmlayer.wf.process.client.transactions.bean.TransactionServiceRequest;
@@ -9,7 +8,6 @@ import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 
 @RegisterRestClient(configKey = "transactions-service-rest-client")
-@ClientHeaderParam(name = "x-api-key", value = "${transactions-service-rest-client.header.x-api-key}") 
 public interface TransactionsServiceRestClient {
     
     @POST
